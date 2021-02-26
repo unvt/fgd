824cc51d7cc76771e051a0b0aebe8af4ae37a1e2
FUJIMURA Hidenori <hfu@users.noreply.github.com>
2021-02-27 08:14:38 +0900

    update

--- stream.rb
+++ stream.rb
@@ -1,5 +1,6 @@
 require 'find'
 require 'zip'
+require 'fileutils'
 
 def entries
   Find.find(SRC_DIR) {|path|
@@ -14,13 +15,32 @@ def entries
 end
 
 def stream
+  list = []
   entries {|path, name|
-    cmd = "ogr2ogr -f GeoJSONSeq /vsistdout/ /vsizip/#{path}/#{name}"
-    sh cmd
+    list << "/vsizip/#{path}/#{name}"
   }
+  cmd = "parallel 'ogr2ogr -f GeoJSONSeq /vsistdout/ {}' ::: #{list.join(' ')}"
+  sh cmd
 end
 
 def produce
+  w1 = File.open('w1.txt', 'w')
+  w2 = File.open('w2.txt', 'w')
+  entries {|path, name|
+    w1 << "/vsizip/#{path}/#{name}\n"
+    w2 << "#{name.split('-')[3]}\n"
+  }
+  w1.close
+  w2.close
+  cmd = "parallel --line-buffer 'ogr2ogr -lco RS=YES -f GeoJSONSeq /vsistdout/ {1} | FGD_LAYER={2} node filter.js' :::: w1.txt ::::+ w2.txt"
+  cmd += " | tippecanoe --no-progress-indicator --no-feature-limit --no-tile-size-limit --simplification=2 --hilbert --minimum-zoom=#{MINZOOM} --maximum-zoom=#{MAXZOOM} --force -o tiles.mbtiles"
+  sh cmd
+  sh "tile-join --force --no-tile-compression --output-to-directory=docs/zxy --no-tile-size-limit tiles.mbtiles"
+  FileUtils.rm('w1.txt')
+  FileUtils.rm('w2.txt')
+end
+
+def produce_no_parallel
   cmd = []
   entries {|path, name|
     cmd << "ogr2ogr -lco RS=YES -f GeoJSONSeq /vsistdout/ /vsizip/#{path}/#{name} | FGD_LAYER=#{name.split('-')[3]} node filter.js"
@@ -29,3 +49,4 @@ def produce
   sh cmd
   sh "tile-join --force --no-tile-compression --output-to-directory=docs/zxy --no-tile-size-limit tiles.mbtiles"
 end
+
